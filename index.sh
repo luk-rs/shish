59b55f79829b419ecea3e1adca6d5fb8ebb99204
luk-rs <santos8@gmail.com>
2023-12-07 22:34:36 +0000

    hello from the cloud

--- index.sh
+++ index.sh
@@ -1,9 +1,17 @@
 #!/bin/sh -e
 
 source_from_remote() {
-  $file="$1"
+#   file="$1"
+#   url='https://github.com/luk-rs/shish/blob/main/index.sh'
 
-}
+#   temp_dir='/tmp'
+#   local_file="$temp_dir/install$file.sh"
+
+#   curl -o "$local_file" "$url"
+#   source_from_remote "$local_file"
+#   rm -rf "$local_file"
+
+# }
 
 if_not_installed() {
   executable="$1"
@@ -25,3 +33,6 @@ is_installed() {
   executable="$1"
   command -v "$executable" >/dev/null 2>&1
 }
+
+# source_from_remote 'luis'
+ echo 'hello from the cloud'
